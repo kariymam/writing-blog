a11dc4ab0c859a9cf994e1d32b1110482b6392d2
kariymam <59487728+kariymam@users.noreply.github.com>
2023-10-03 00:18:00 -0400

    postlist

--- _includes/utilis.js
+++ _includes/utilis.js
@@ -1,6 +1,8 @@
 /*     navigation    */
 const indicator = document.querySelector('.indicator'); // target element
 const navLinks = document.querySelectorAll('.transition'); // Get references to the hoverable
+// const prop = (k, v) => style.setProperty(k, v)
+// prop("-webkit-transform", `translate(${l}px)`);
 
 // for each element with transition class
 navLinks.forEach(transition => {
@@ -8,7 +10,7 @@ navLinks.forEach(transition => {
 		const l = transition.offsetLeft;
 		const w = transition.offsetWidth;
 		if (transition.id) {
-			indicator.style.transform = `translate(${l}px)`; // working
+			indicator.style.webkitTransform = `translate(${l}px)`; // working
 			indicator.style.width = `${w}px`; // working
 		} else { }
 			indicator.style.opacity = "1";
---
+++ content/blog/Third post.md
@@ -0,0 +1,12 @@
+---
+title: test
+description: This is a description
+date: "git Last Modified"
+tags:
+  - exhibition-review
+---
+
+## A note about my "case study" style
+
+1. If you're looking for post-its, FigJams, Figma prototypes, or screenshots of any proprietary information I would've created for Capital One decks that would be locked down behind a corporate intranet, well... I'm sorry, they're just not here. I couldn't bust them out of the firewall. All I can try to do for you, dear reader, is share what I did and some anonymous feedback I received during my time.
+2. I'm going off of my memories, so details are hazy as I can't remember what I ate for breakfast three days ago.
